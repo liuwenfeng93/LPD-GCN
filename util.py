89c201b1b8259f7789c376c238fc0253f96f2173
刘文枫 <72639248+liuwenfeng93@users.noreply.github.com>
2020-10-13 22:16:56 +0800

    Update util.py

--- util.py
+++ util.py
@@ -36,7 +36,7 @@ def load_data(dataset, degree_as_tag):
     label_dict = {}
     feat_dict = {}
 
-    with open('dataset/%s/%s.txt' % (dataset, dataset), 'r') as f:
+    with open('datasets/%s/%s.txt' % (dataset, dataset), 'r') as f:
         n_g = int(f.readline().strip())
         for i in range(n_g):
             row = f.readline().strip().split()
